5375752bdd1c07a361431c3e5e098c05557c2e61
lachonman <korzeniowski.renard@gmail.com>
2020-11-10 17:42:17 +0100

    Fixing bugs

--- src/pipelines/pipeline.py
+++ src/pipelines/pipeline.py
@@ -128,7 +128,7 @@ class BlurrPipeline:
     @classmethod
     def load_data(cls, dataset, train_samples):
         data_train = datasets.load_dataset(dataset, split="train")
-        data_test = datasets.load_dataset(dataset, split="test")
+        data_test = datasets.load_dataset(dataset, split="validation")
         df_train = pd.DataFrame(data_train)
         df_test = pd.DataFrame(data_test)
         if train_samples != "all":
