90744dbbdfa37c655054792fc016053d29a82e70
Przemyslaw Kaleta <przemek2889@gmail.com>
2020-11-13 18:23:24 +0100

    Decreased num workerks to 1.

--- run_classification_experiments.py
+++ run_classification_experiments.py
@@ -84,7 +84,7 @@ def run_exp():
                 metric_for_best_model="eval_accuracy",
                 remove_unused_columns=False,
                 no_cuda=False,
-                dataloader_num_workers=2
+                dataloader_num_workers=1
             )
 
             trainer = Trainer(
