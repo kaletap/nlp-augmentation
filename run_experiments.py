e9827f08d974f05c651413eff99046a622598df2
lachonman <korzeniowski.renard@gmail.com>
2020-11-17 09:20:48 +0100

    Updated order of experiment execution

--- run_experiments.py
+++ run_experiments.py
@@ -18,12 +18,12 @@ def fill_paths(task, train_samples, aug, seed, config):
 
 def run_exp(task, main_config):
     pipe_cls, og_config = main_config["tasks"][task]
-    for aug in main_config["augmentations"]:
-        og_config["augmentation"] = aug
+    for seed in main_config["seeds"]:
+        og_config["seed"] = seed
         for train_samples in main_config["train_samples"]:
             og_config["train_samples"] = train_samples
-            for seed in main_config["seeds"]:
-                og_config["seed"] = seed
+            for aug in main_config["augmentations"]:
+                og_config["augmentation"] = aug
                 config = deepcopy(og_config)
                 config = fill_paths(task, train_samples, aug, seed, config)
                 pipe = pipe_cls.from_name(exp_parameters=config)
@@ -31,5 +31,5 @@ def run_exp(task, main_config):
 
 
 if __name__ == "__main__":
-    # run_exp(task="qa", main_config=configs.experiments_setup)
-    run_exp(task="summarization", main_config=configs.experiments_setup)
+    run_exp(task="qa", main_config=configs.experiments_setup)
+    # run_exp(task="summarization", main_config=configs.experiments_setup)
