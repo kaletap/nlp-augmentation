7a6488cb58d959667572ccc1e4d30777cfee826a
lachonman <korzeniowski.renard@gmail.com>
2020-11-19 17:34:34 +0100

    Data pretraining data augmentation scripts

--- download_data.py
+++ download_data.py
@@ -8,36 +8,37 @@ PARAMS_LIST = [
         "squad_v2",
         "train",
         [
-             # (100, ("context",), {"type": "no_aug", "repeat": 50}),
-             # (1000, ("context",), {"type": "no_aug", "repeat": 5}),
-             # (100, ("context",), {"type": "rules", "repeat": 50}),
-             # (1000, ("context",), {"type": "rules", "repeat": 5}),
-             (100, ("context",), {"type": "LM", "repeat": 50}),
-             (1000, ("context",), {"type": "LM", "repeat": 5})
+             # (100, "qa", ("context",), {"type": "no_aug", "repeat": 50}),
+             # (1000, "qa", ("context",), {"type": "no_aug", "repeat": 5}),
+             # (10, "qa", ("context",), {"type": "rules", "repeat": 50}),
+             # (1000, "qa", ("context",), {"type": "rules", "repeat": 5}),
+             (100, "qa", ("context",), {"type": "LM", "repeat": 100}),
+             (1000, "qa", ("context",), {"type": "LM", "repeat": 10}),
+             (5000, "qa", ("context",), {"type": "LM", "repeat": 2}),
         ],
     ],
-    [
-        "squad_v2",
-        "validation",
-        [
-            # ("all", ("context",), {"type": "no_aug", "repeat": 1}),
-            # ("all", ("context",), {"type": "no_aug", "repeat": 1})
-        ],
-    ]
+    # [
+    #     "squad_v2",
+    #     "validation",
+    #     [
+    #         ("all", "qa", ("context",), {"type": "no_aug", "repeat": 1}),
+    #         ("all", "qa", ("context",), {"type": "no_aug", "repeat": 1})
+    #     ],
+    # ]
 ]
 
 
 for (dataset_name, split, ds_params) in PARAMS_LIST:
     ds = datasets.load_dataset(dataset_name, split=split)
     df = pd.DataFrame(ds)
-    for sample_count, x_cols, aug in ds_params:
+    for sample_count, task, x_cols, aug in ds_params:
         print(f"start processing {aug['type']}, sample_count {sample_count}")
         if isinstance(sample_count, int):
-            df_subset = df[:sample_count] # make sure its a copy and does not modify original dataset
+            df_subset = df[:sample_count]
             print(sample_count)
         else:
             df_subset = df
             print("not trimming ds cuz sample_count: " + sample_count)
         auged_df = data_downloading.augment_data(
-            df_subset, aug_type=aug["type"], aug_repeat=aug["repeat"], x_cols=x_cols)
+            df_subset, task=task, aug_type=aug["type"], aug_repeat=aug["repeat"], x_cols=x_cols)
         auged_df.to_csv(f"{dataset_name}_{split}_{sample_count}_aug-{aug['type']}_repeat-{aug['repeat']}.csv")
--- src/training_utils/data_downloading.py
+++ src/training_utils/data_downloading.py
@@ -8,21 +8,43 @@ def get_augmentation_fn(augmenter, x_cols):
     return augmenter(x_cols)
 
 
-def augment_data(df, aug_type, aug_repeat, x_cols):
+def augment_data(df, task, aug_type, aug_repeat, x_cols):
     if aug_type != "no_aug":
         augmenter = augmentations.get_augmentation_fn(aug_type, wrapped=False)
         aug_df = pd.concat([df] * (aug_repeat - 1))
-        aug_df = augment_text_df(aug_df, x_cols, augmenter)
+        augment_text_df(aug_df, task, x_cols, augmenter)
         df = pd.concat([df, aug_df])
     else:
         df = pd.concat([df] * aug_repeat)
     return df
 
 
-def augment_text_df(df, x_cols, aug_fn):
-    import pdb;pdb.set_trace()
+def augment_qa(df, idx, row, col, aug_fn):
+    assert len(row["answers"]["answer_start"]) == 1, "There is more than one answer to one" \
+                                                     "context. It's not supported by this preprocessing"
+    answer = row["answers"]
+    answer_start, answer_txt = answer["answer_start"][0], answer["text"][0]
+    answer_end = answer_start + len(answer_txt)
+
+    context_first_part = row[col][:answer_start].strip()
+    context_second_part = row[col][answer_end:].strip()
+
+    aug_context_first_part = aug_fn(context_first_part) + " "
+    aug_context_second_part = " " + aug_fn(context_second_part)
+
+    answer_start = len(aug_context_first_part)
+    auged_txt = aug_context_first_part + answer_txt + aug_context_second_part
+
+    answer["answer_start"][0] = answer_start
+    df.loc[idx, col] = auged_txt
+    return df
+
+
+def augment_text_df(df, task, x_cols, aug_fn):
     for idx, row in tqdm(df.iterrows()):
         for col in x_cols:
-            auged_text = aug_fn(row[col])
-            df.loc[idx, col] = auged_text
+            if task == "qa":
+                df = augment_qa(df, idx, row, col, aug_fn)
+            else:
+                raise ValueError(f"Task type {task} is not supported")
     return df
