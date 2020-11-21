169ffcab0cce6360e35036b6eacc6f23b2c6c2e8
lachonman <korzeniowski.renard@gmail.com>
2020-11-21 12:08:42 +0100

    Updated config

--- src/pipelines/configs.py
+++ src/pipelines/configs.py
@@ -41,7 +41,7 @@ summary_bart_config = {
     "pre_config_overwrite": {'max_length': 130, 'min_length': 30},
     "train_params": {
         "all": {
-            "epochs": (12,),
+            "epochs": (6,),
             "unfreeze": (),
             "lr": (),
         },
@@ -59,7 +59,7 @@ qa_bert_config = {
     "pre_config_overwrite": {},
     "train_params": {
         "all": {
-            "epochs": (12,),
+            "epochs": (6,),
             "unfreeze": (),
             "lr": (),
         },
