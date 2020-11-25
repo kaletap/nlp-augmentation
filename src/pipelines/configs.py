d143159d63e6cece51253904734036b588f5d778
Przemyslaw Kaleta <przemek2889@gmail.com>
2020-11-25 10:39:08 +0100

    SNLI 2500

--- src/pipelines/configs.py
+++ src/pipelines/configs.py
@@ -105,7 +105,7 @@ ag_news_config = {
     "eval_batch_size": 8,
     "gradient_accumulation_steps": 2,
     "metrics_function": compute_multiclass_metrics,
-    "train_sizes": [20, 100, 1000, 2_500]
+    "train_sizes": []
 }
 
 imdb_config = {
@@ -121,7 +121,7 @@ imdb_config = {
     "eval_batch_size": 8,
     "gradient_accumulation_steps": 2,
     "metrics_function": compute_binary_metrics,
-    "train_sizes": [20, 100, 1000]
+    "train_sizes": []
 }
 
 snli_config = {
@@ -137,7 +137,7 @@ snli_config = {
     "eval_batch_size": 16,
     "gradient_accumulation_steps": 1,
     "metrics_function": compute_multiclass_metrics,
-    "train_sizes": [20, 100, 1000]
+    "train_sizes": [2500]
 }
 
 twitter_config = {
