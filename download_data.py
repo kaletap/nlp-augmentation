89917d0be81421be145a2658366f36fde43bf3dc
lachonman <korzeniowski.renard@gmail.com>
2020-12-04 16:58:59 +0100

    Data download for prop check

--- download_data.py
+++ download_data.py
@@ -36,7 +36,7 @@ PARAMS_LIST = [
              # (5000, "qa", ("context",), {"type": "no_aug", "repeat": 2}),
              # (100, "qa", ("context",), {"type": "rules", "repeat": 100}),
              # (1000, "qa", ("context",), {"type": "rules", "repeat": 10}),
-             # (5000, "qa", ("context",), {"type": "rules", "repeat": 2}),
+             (5000, "qa", ("context",), {"type": "rules", "repeat": 10}),
              # (100, "qa", ("context",), {"type": "LM", "repeat": 100}), # 100 original and 9900 augmented
              # (1000, "qa", ("context",), {"type": "LM", "repeat": 10}), # 1000 original and 9000 augmented
              (5000, "qa", ("context",), {"type": "LM", "repeat": 10}), # 5000 original and 5000 augmented
