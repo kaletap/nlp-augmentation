7391ae8ee71c320f6ae46f922c2be4d7e77e9a88
lachonman <korzeniowski.renard@gmail.com>
2020-12-05 22:22:02 +0100

    Update config for prop check

--- src/pipelines/configs.py
+++ src/pipelines/configs.py
@@ -117,9 +117,9 @@ common_config = {
 }
 
 experiments_setup = {
-    "train_samples": ((100, 100), (1000, 10), (5000, 2)), #["all", 10, 100, 1000, 10000], # (org_smpl_count, aug_repeat)
-    "augmentations": ("rules", "no_aug", "LM"),# "vae", "rules", "style_transfer"],
-    "seeds": (21, 37),# 9, 11, 21, 37]
+    "train_samples": ((5000, 100), (5000, 1000), (5000, 2500), (5000, 5000), (5000, 7500), (5000, 10000), (5000, 25000), (5000, 50000)), #["all", 10, 100, 1000, 10000], # (org_smpl_count, aug_repeat)
+    "augmentations": ("rules", "LM"),# "vae", "rules", "style_transfer"],
+    "seeds": (9, ),# 9, 11, 21, 37]
     "tasks": {
         "summarization": ((pipeline.SummarizationPipeline, {**summary_bart_config, **cnn_dailymail_config, **common_config})),
         "qa": ((pipeline.QuestionAnsweringPipeline, {**qa_xlm_config, **squad_v2_config, **common_config}))
