200443597a91d3a2509c77aab1901e1efb54073f
lachonman <korzeniowski.renard@gmail.com>
2020-12-12 15:56:02 +0100

    Fix covid preprocessing

--- src/pipelines/configs.py
+++ src/pipelines/configs.py
@@ -124,9 +124,9 @@ common_config = {
 }
 
 experiments_setup = {
-    "train_samples": ((100, 100), (500, 20), (1500, 7)), # (org_smpl_count, aug_repeat) # (5000, 100), (5000, 1000), (5000, 2500), (5000, 5000), (5000, 7500), (5000, 10000), (5000, 25000), (5000, 50000)), #
+    "train_samples": ((100, 100), (500, 20)), # (1500, 7) # (org_smpl_count, aug_repeat) # (5000, 100), (5000, 1000), (5000, 2500), (5000, 5000), (5000, 7500), (5000, 10000), (5000, 25000), (5000, 50000)), #
     "augmentations": ("no_aug", "rules", "LM"),# "vae", "rules", "style_transfer"],
-    "seeds": (9, 11),# 9, 11, 21, 37]
+    "seeds": (9,),# 9, 11, 21, 37]
     "tasks": {
         "summarization": ((pipeline.SummarizationPipeline, {**summary_bart_config, **cnn_dailymail_config, **common_config})),
         "qa": ((pipeline.QuestionAnsweringPipeline, {**qa_bert_config, **squad_v2_config, **common_config})),
