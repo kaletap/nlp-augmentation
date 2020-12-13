473530e9a49ab5670d676f084436dcb30830029c
Przemyslaw Kaleta <przemek2889@gmail.com>
2020-12-13 15:35:43 +0100

    Running multiple experiments for small datasets in order to calculate sd.

--- run_classification_experiments.py
+++ run_classification_experiments.py
@@ -30,8 +30,10 @@ ROOT_TRAINING_CSV_PATH = root_training_csv_path[PLATFORM]
 SAVE_DIR = "."
 # USE_FINETUNED_MODEL_FOR_CLASSIFICATION = True
 TRANSFORMER_MODEL_NAME = "roberta-base"  # "albert-base-v2"
+N_EXPERIMENTS = 5
 
 
+# TODO: reorganize this code into neat functions
 def run_exp():
     tokenizer = AutoTokenizer.from_pretrained(TRANSFORMER_MODEL_NAME, use_fast=False)
 
@@ -58,93 +60,94 @@ def run_exp():
             for USE_FINETUNED_MODEL_FOR_CLASSIFICATION in (True, ):
                 data_collator = DataCollator(tokenizer, text_colname="text", label_colname=config["label_colname"])
                 for train_size in config["train_sizes"]:
-                    if USE_FINETUNED_MODEL_FOR_CLASSIFICATION:  # we want to load a new model for each train_size!
-                        model_name_or_path = os.path.join(ROOT_MLM_DIR, mlm_relative_path)
-                        print(f"Loading model for classification from {model_name_or_path}")
-                    else:
-                        model_name_or_path = TRANSFORMER_MODEL_NAME
-                    model = AutoModelForSequenceClassification.from_pretrained(model_name_or_path, return_dict=True,
-                                                                               num_labels=config["num_labels"])
-                    training_csv_path = os.path.join(ROOT_TRAINING_CSV_PATH, name, augmentation_name, f"{train_size}.csv")
-                    print("Augmenter", augmenter)
-                    train_dataset, val_dataset, test_dataset = get_datasets(
-                        config["dataset_name"],
-                        augmenter=augmenter,
-                        train_size=train_size,
-                        val_size=config["val_size"],
-                        test_size=config["test_size"],
-                        augmentation_prob=augmentation_config["augmentation_prob"],
-                        load_test=config["load_test"],
-                        text_columns=config["text_colname"],
-                        sep_token=tokenizer.sep_token,
-                        training_csv_path=training_csv_path
-                    )
-                    print("Time:", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-                    print(f"Train size: {len(train_dataset)}, Validation size: {len(val_dataset)}, Test size: {len(test_dataset)}")
-                    print(train_dataset[0])
-                    print(val_dataset[0])
-                    print(test_dataset[0])
+                    for _ in range(N_EXPERIMENTS):
+                        if USE_FINETUNED_MODEL_FOR_CLASSIFICATION:  # we want to load a new model for each train_size!
+                            model_name_or_path = os.path.join(ROOT_MLM_DIR, mlm_relative_path)
+                            print(f"Loading model for classification from {model_name_or_path}")
+                        else:
+                            model_name_or_path = TRANSFORMER_MODEL_NAME
+                        model = AutoModelForSequenceClassification.from_pretrained(model_name_or_path, return_dict=True,
+                                                                                   num_labels=config["num_labels"])
+                        training_csv_path = os.path.join(ROOT_TRAINING_CSV_PATH, name, augmentation_name, f"{train_size}.csv")
+                        print("Augmenter", augmenter)
+                        train_dataset, val_dataset, test_dataset = get_datasets(
+                            config["dataset_name"],
+                            augmenter=augmenter,
+                            train_size=train_size,
+                            val_size=config["val_size"],
+                            test_size=config["test_size"],
+                            augmentation_prob=augmentation_config["augmentation_prob"],
+                            load_test=config["load_test"],
+                            text_columns=config["text_colname"],
+                            sep_token=tokenizer.sep_token,
+                            training_csv_path=training_csv_path
+                        )
+                        print("Time:", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                        print(f"Train size: {len(train_dataset)}, Validation size: {len(val_dataset)}, Test size: {len(test_dataset)}")
+                        print(train_dataset[0])
+                        print(val_dataset[0])
+                        print(test_dataset[0])
 
-                    if ROOT_TRAINING_CSV_PATH:  # less epochs since we have a lot of data
-                        num_train_epochs = 2
-                    else:
-                        num_train_epochs = {
-                            20: 10,
-                            100: 10,
-                            1000: 7,
-                            2_500: 6,
-                            10_000: 6,
-                            100_000: 3
-                        }.get(train_size, 6)
-                        if train_size > 50_000:
-                            num_train_epochs = 3
+                        if ROOT_TRAINING_CSV_PATH:  # less epochs since we have a lot of data
+                            num_train_epochs = 2
+                        else:
+                            num_train_epochs = {
+                                20: 10,
+                                100: 10,
+                                1000: 7,
+                                2_500: 6,
+                                10_000: 6,
+                                100_000: 3
+                            }.get(train_size, 6)
+                            if train_size > 50_000:
+                                num_train_epochs = 3
 
-                    output_dir = os.path.join(ROOT_OUTPUT_DIR, f'{name}_{augmentation_config["name"]}_train_size_{train_size}')
-                    # https://huggingface.co/transformers/main_classes/trainer.html#trainingarguments
-                    training_args = TrainingArguments(
-                        output_dir=output_dir,
-                        logging_dir=SAVE_DIR,
-                        num_train_epochs=num_train_epochs,
-                        learning_rate=4e-5,
-                        weight_decay=0.01,
-                        per_device_train_batch_size=config["batch_size"],
-                        per_device_eval_batch_size=config["eval_batch_size"],
-                        gradient_accumulation_steps=config["gradient_accumulation_steps"],
-                        warmup_steps=0,
-                        logging_steps=50,
-                        load_best_model_at_end=True,
-                        evaluation_strategy='epoch',
-                        metric_for_best_model="eval_accuracy",
-                        remove_unused_columns=False,
-                        no_cuda=False,
-                        dataloader_num_workers=0
-                    )
+                        output_dir = os.path.join(ROOT_OUTPUT_DIR, f'{name}_{augmentation_config["name"]}_train_size_{train_size}')
+                        # https://huggingface.co/transformers/main_classes/trainer.html#trainingarguments
+                        training_args = TrainingArguments(
+                            output_dir=output_dir,
+                            logging_dir=SAVE_DIR,
+                            num_train_epochs=num_train_epochs,
+                            learning_rate=4e-5,
+                            weight_decay=0.01,
+                            per_device_train_batch_size=config["batch_size"],
+                            per_device_eval_batch_size=config["eval_batch_size"],
+                            gradient_accumulation_steps=config["gradient_accumulation_steps"],
+                            warmup_steps=0,
+                            logging_steps=50,
+                            load_best_model_at_end=True,
+                            evaluation_strategy='epoch',
+                            metric_for_best_model="eval_accuracy",
+                            remove_unused_columns=False,
+                            no_cuda=False,
+                            dataloader_num_workers=0
+                        )
 
-                    trainer = Trainer(
-                        model=model,
-                        args=training_args,
-                        train_dataset=train_dataset,
-                        eval_dataset=val_dataset,
-                        data_collator=data_collator,
-                        compute_metrics=config["metrics_function"]
-                    )
+                        trainer = Trainer(
+                            model=model,
+                            args=training_args,
+                            train_dataset=train_dataset,
+                            eval_dataset=val_dataset,
+                            data_collator=data_collator,
+                            compute_metrics=config["metrics_function"]
+                        )
 
-                    trainer.train()
+                        trainer.train()
 
-                    test_result = trainer.evaluate(test_dataset)
-                    print(test_result)
-                    results_df = results_df.append({
-                        "dataset": name,
-                        "augmentation": augmentation_config["name"],
-                        "training_size": train_size,
-                        "accuracy": test_result["eval_accuracy"],
-                        "finetuned_clf": USE_FINETUNED_MODEL_FOR_CLASSIFICATION
-                    }, ignore_index=True)
-                    results_df.to_csv(os.path.join(ROOT_OUTPUT_DIR, 'results.csv'), index=False)
-                    print(results_df)
-                    # cleanup
-                    shutil.rmtree(output_dir, ignore_errors=True)
-                    print()
+                        test_result = trainer.evaluate(test_dataset)
+                        print(test_result)
+                        results_df = results_df.append({
+                            "dataset": name,
+                            "augmentation": augmentation_config["name"],
+                            "training_size": train_size,
+                            "accuracy": test_result["eval_accuracy"],
+                            "finetuned_clf": USE_FINETUNED_MODEL_FOR_CLASSIFICATION
+                        }, ignore_index=True)
+                        results_df.to_csv(os.path.join(ROOT_OUTPUT_DIR, 'results.csv'), index=False)
+                        print(results_df)
+                        # cleanup
+                        shutil.rmtree(output_dir, ignore_errors=True)
+                        print()
 
 
 if __name__ == "__main__":
